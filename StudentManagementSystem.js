155c3371c1cf545605556ca72fe9bd6c575ed7b7
Youssef Efrit <youssef.efrit2@gmail.com>
2021-04-06 13:28:37 +0200

    add student fucntion

--- StudentManagementSystem.js
+++ StudentManagementSystem.js
@@ -2,3 +2,11 @@ function add (number1, number2){
     return number1 + number2
 };
 add(1,2)
+
+var numberofStudents = 0 ;
+
+function addStudent () {
+
+    numberofStudents = numberofStudents + 1 ;
+}
+
